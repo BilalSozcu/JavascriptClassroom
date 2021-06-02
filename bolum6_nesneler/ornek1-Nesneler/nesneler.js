c608c33f35943ee67c8fd291ed0f490edcaa600c
Bilal <bilalsozcu.57@gmail.com>
2021-06-02 11:34:48 +0200

    nesneler

---
+++ bolum6_nesneler/ornek1-Nesneler/nesneler.js
@@ -0,0 +1,148 @@
+// ======================================================
+//            NESNELER (OBJECTS)
+//======================================================
+// Diziler sıralı bellek bölgeleridir. Dolayısıyla dizilere erişim indisleme ile yapılır.
+// Sıralı belleklere ihtiyaç varsa (structred) dizi kullanmak mantıklıdır.
+// Eğer veriler kompleks ise o zaman dizlerde erişim güçlüğü ortaya çıkar.
+// ! OBJECT
+// Nesnelerde Key-value (property-value) yapısı kullanlır.
+// Nesnelerde aradıgımız veriyi erişmek için property adini kullanmak yeterlidir.
+// Javascript proto-type bir dildir. Java class-based bir dildir.
+
+//  ! Object literal
+const insan = {
+  ad: "Can",
+  soyad: "Yilmaz",
+  yas: 30,
+  meslek: "developer",
+  diller: ["Java", "JS", "C++", "SQL", "Python"],
+};
+console.log(insan);
+console.log(insan.ad); // .dot notasyonu ile verilere erisilebilir.
+
+// " " isareti olmasa da key'ler string mantigiyla calisir
+// Koseli parantez erisimi(koseli parantez icerisine nir expression yazmak mumkundur)
+console.log(insan["yas"]);
+const yazi = "Adim";
+console.log(`${yazi} ${insan.ad} ve yasim ${insan["yas"]}'dur`);
+
+const bilgiSecimi = prompt(
+  "Insan hakkinda ne bilmek istersin ? Seciminizi giriniz: ad, soyad, yas, meslek ve diller"
+);
+
+// console.log(insan[bilgiSecimi]); // undefined herhangi bir veri gir
+
+if (insan[bilgiSecimi]) {
+  console.log(insan[bilgiSecimi]);
+} else {
+  console.log("Yanlis secim");
+}
+insan.konum = "Turkiye";
+insan.email = "insan@gmail.com";
+insan["dogumTarihi"] = 1990;
+console.log(insan);
+
+// ************  NESNE METHODLARI  ************
+
+const kisi = {
+  ad: "Ahmet",
+  soyad: "Can",
+  dogumTarihi: 1990,
+  meslek: "tester",
+  tool: "selenium",
+  ehliyet: true,
+  yasHesapla: function () {
+    console.log(this);
+    return new Date().getFullYear() - this.dogumTarihi;
+  },
+  ozet: function () {
+    return `${this.ad} ${this.soyad} ${this.yasHesapla()} yasindadir. Meslegi ${
+      this.meslek
+    }'likdir.`;
+  },
+};
+
+// ? Arrow function'lar (lambda fonsiyonlari) icerisinde new veya this gibi
+// ? function expression yerine arrow function yazilirsa NaN doner
+// ! mülakat sorusu: lambda veya arrow fonksiyonlari klasik obje degiller.
+// ! klasik obje metodlarini this new gibi metodlar kullanilamaz
+
+const tarih = new Date();
+console.log(tarih);
+console.log(tarih.getMonth());
+console.log(tarih.getDay());
+console.log(kisi.yasHesapla());
+console.log(kisi.ozet());
+
+// *************  NESNE ITERASYON ORNEKLERI  **************
+
+const kisiler = [
+  {
+    ad: "mustafa",
+    soyad: "can",
+    meslek: "developer",
+    yas: 20,
+  },
+  {
+    ad: "ayse",
+    soyad: "yilmaz",
+    meslek: "tester",
+    yas: 25,
+  },
+  {
+    ad: "canan",
+    soyad: "ari",
+    meslek: "grafiker",
+    yas: 35,
+  },
+  {
+    ad: "ali",
+    soyad: "veli",
+    meslek: "team lead",
+    yas: 26,
+  },
+  {
+    ad: "ceren",
+    soyad: "yilmaz",
+    meslek: "developer",
+    yas: 24,
+  },
+];
+
+console.log(kisiler);
+
+// ORNEK 1- Kisiler dizisindeki kisilerin mesleklerini konsola yazdiralim
+kisiler.forEach((x) => console.log(x.meslek));
+
+//ÖRNEK 2- kisiler dizisindeki tüm bireylerin yaşını bir arttırarak yazdırınız.
+const yaslar = kisiler.map((x) => x.yas + 1);
+console.log(yaslar);
+
+// ORNEK 3- Kisiler dizisindeki kişilerin isimlerini büyük harf olarak değiştiren ve
+// yaşlarini da 5 artırak yeni bir nesne oluşturan kodu yaziniz.
+const buyukHarf = kisiler.map((x) => {
+  return {
+    ad: x.ad.toUpperCase(),
+    yas: x.yas + 5,
+  };
+});
+console.log(buyukHarf);
+
+// ORNEK 4- Yaşı 25' eşit veya küçük olanların adlarını yazdıran kodu yazınız.
+kisiler.filter((x) => x.yas <= 25).forEach((x) => console.log(x.ad));
+
+// ORNEK 5- Mesleği developer olanların isim ve yaşlarını bir diziye kaydediniz.
+const developer = kisiler
+  .filter((x) => x.meslek == "developer")
+  .map((x) => {
+    return {
+      ad: x.ad,
+      yas: x.yas,
+    };
+  });
+console.log(developer);
+
+// ORNEK 6- kisilerin ortalama yasini hesaplayiniz.
+const ortalamaYas =
+  kisiler.reduce((toplam, kisi) => toplam + kisi.yas, 0) / kisiler.length;
+console.log(ortalamaYas);
---
+++ bolum6_nesneler/ornek1-Nesneler/yeniNesilOperatorler.js
@@ -0,0 +1,78 @@
+// ======================================================
+//      YENİ NESİL OPERATORLER: DESTRUCTURING (OBJECT)
+// ======================================================
+// ES6 ile birlikte bir dizi ya da nesnenin içerisindeki
+// elemanlar kolaylıkla ayrıştırılabilir.
+
+const birey = {
+  ad: "Can",
+  konum: "Samsun",
+  email: "can@gmail.com",
+};
+
+// 1.Yontem ==> Klasik yontem
+let ad1 = birey.ad;
+let konum1 = birey.konum;
+
+console.log(ad1, konum1);
+
+ad1 = "Canan";
+console.log(birey);
+
+// 2. YONTEM (OBJECT DESTRUCTURING)
+//  ! Destructuring kullanilirken "Key" isimleri nesneninki ile ayni olmalidir
+//  ! aksi takdirde undefined degerler donerler
+const { ad, konum, email } = birey; // birey nesnesindeki property'leri ayristirir
+console.log(ad, konum, email);
+// ad = "Mustafa";
+birey.ad = "Mustafa"; // birey nesnesindeki ad'i gunceller
+console.log(birey);
+
+function bilgiAl() {
+  return {
+    id: "101",
+    urunAdi: "Apple",
+    fiyat: 15000,
+  };
+}
+console.log(bilgiAl());
+const { id, urunAdi, fiyat } = bilgiAl();
+
+// ======================================================
+//      YENİ NESİL OPERATORLER: DESTRUCTURING (ARRAY)
+// ======================================================
+const isimler = ["Ahmet", "Can", "Veli"];
+
+// Klasik Yontem
+const isim1 = isimler[0];
+const isim2 = isimler[1];
+
+//  Dest
+const [kisi1, kisi3] = isimler;
+console.log(kisi1, kisi3);
+
+// ======================================================
+//        YENİ NESİL OPERATORLER: SPREAD VEYA REST
+// ======================================================
+// Spread operatoru dizileri ve nesneleri birleştirmek
+// veya ayırmak icin kullanilabilir.
+// Kullanmak için dizinin onune ... karakterleri konulur.
+// DİZİLERDE DESTRUCTURING YAPARKEN GERİ KALANLARI SEÇMEK İÇİN KULLANILABİLİR.
+const arabalar = ["bmw", "fiat", "citroen", "reno", "audi"];
+const [araba1, araba2, ...geriKalanArabalar] = arabalar;
+console.log(araba1, geriKalanArabalar);
+// NESNELERDE DESTRUCTURING YAPARKEN GERİ KALANLARI SEÇMEK İÇİN KULLANILABİLİR.
+const personel = { ad: "john", soyad: "smith", meslek: "developer" };
+const { meslek, ...geriKalan } = personel;
+console.log(meslek, geriKalan);
+// DIZILERI BIRLESTIRMEK ICIN DE KULLANILABILIR
+const araclar = ["Ucak", "Helikopter", "Bisiklet"];
+const otomobiller = ["Kamyon", "Kamyonet", "Otobus", "Araba"];
+const birlesim = ["Ucak", "Helikopter", "Bisiklet", ...otomobiller];
+console.log(birlesim);
+const birlesim1 = [...araclar, ...otomobiller];
+console.log(birlesim1);
+
+const meyveler = ["kiraz", "armut", "seftali", "uzum"];
+const turunclar = ["limon", "portakal", "mandalina", ...meyveler];
+console.log(turunclar);
